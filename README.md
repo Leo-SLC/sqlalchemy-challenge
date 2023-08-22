4e2318aa01c8eecffc60a30f1cb22ddf463bcbdd
Leo-SLC <leoantinozzi@Leos-MacBook-Pro.local>
2023-08-22 17:31:51 -0600

    Working File

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # sqlalchemy-challenge
+
+Help with this assignment came from my Tutor Geronimo Perez, as well as additional resources such as:
+
+https://github.com/Deving789/surfs_up/
+
+https://www.sqlitetutorial.net/
+
+https://github.com/Shannon-Goddard/Goddard_Shannon_surfs_up/
--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,6 +1,16 @@
 # Import the dependencies.
 from flask import Flask
 
+import pandas as pd
+import numpy as np
+import datetime as dt
+import sqlalchemy
+
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify
+
 
 #################################################
 # Database Setup
