9d6f80c3cf4b7f1877b03c3527854f18d2772f2a
Leo-SLC <leoantinozzi@Leos-MacBook-Pro.local>
2023-08-22 19:07:04 -0600

    Project work

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -15,18 +15,21 @@ from flask import Flask, jsonify
 #################################################
 # Database Setup
 #################################################
-
+engine = create_engine("sqlite:///../Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
+Base = automap_base()
+
 
 # reflect the tables
-
+Base.prepare(autoload_with=engine)
 
 # Save references to each table
-
+measurement = Base.classes.measurement
+station = Base.classes.station
 
 # Create our session (link) from Python to the DB
-
+session = Session(engine)
 
 #################################################
 # Flask Setup
@@ -40,7 +43,44 @@ app = Flask(__name__)
 #################################################
 @app.route('/')
 def home():
-    return '<h1>Hello world!!!</h1>'
+    return f'''
+        <h1>Climate App</h1>
+        <h4>Check out the following routes:</h4>
+        <ul>
+            <li>/api/v1.0/precipitation</li>
+            <li>/api/v1.0/stations</li>
+            <li>/api/v1.0/tobs</li>
+            <li>/api/v1.0/[startDate]</li>
+            <li>/api/v1.0/[startDate]/[endDate]</li>
+        </ul>'''
+
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    session = Session(engine)
+    return {date:prcp for date,prcp in session.query(measurement.date, measurement.prcp).filter(measurement.date>="2016-08-23").all()}
+
+@app.route("/api/v1.0/stations")
+def stations():
+    session = Session(engine)
+    return {id:loc for id,loc in session.query(station.station, station.name).all()}
+
+@app.route("/api/v1.0/tobs")
+def tempsobs():
+    session = Session(engine)
+    return {date:tobs for date,tobs in session.query(measurement.date, measurement.tobs).filter(((measurement.date>="2016-08-23"))&(measurement.station == "USC00519281")).all()}
+
+@app.route("/api/v1.0/<startDate>")
+@app.route("/api/v1.0/<startDate>/<endDate>")
+def startend(startDate, endDate="2017-08-23"):
+    results = session.query(func.min(measurement.tobs), 
+        func.avg(measurement.tobs), 
+        func.max(measurement.tobs)).\
+        filter((measurement.date>=startDate)&(measurement.date<=endDate)).first()
+
+    print(results)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
